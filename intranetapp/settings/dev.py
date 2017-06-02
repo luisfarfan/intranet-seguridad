7c35e25b50ac65255a13bd00d45a00908913b36d
Lucho <lucho.farfan9@gmail.com>
2017-06-02 15:49:16 -0500

    added base

--- intranetapp/settings/dev.py
+++ intranetapp/settings/dev.py
@@ -9,8 +9,15 @@ ALLOWED_HOSTS = ['*']
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'sql_server.pyodbc',
+        'NAME': 'CPV2017_SEGURIDAD',
+        'USER': 'lfarfan',
+        'PASSWORD': 'lfarfan',
+        'HOST': '172.18.1.41',
+        'OPTIONS': {
+            'driver': 'SQL Server',
+            'unicode_results': True
+        },
     },
     'consecucion': {
         'ENGINE': 'sql_server.pyodbc',
